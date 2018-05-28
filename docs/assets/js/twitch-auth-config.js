3b8a0865ba5e4da789f758ea6851a89a0ebb026f
Eric Ahn <wchill1337@gmail.com>
2018-05-28 12:52:20 -0700

    Change redirect Url

--- docs/assets/js/twitch-auth-config.js
+++ docs/assets/js/twitch-auth-config.js
@@ -1,5 +1,5 @@
 // CONFIG: twitch oauth settings
 window.twitch_auth_config = {
     clientId: '6ilamg1dh1d2fwi30x5ryiarfq6y86', // from [Twitch Client ID](https://glass.twitch.tv/console/apps)
-    redirect: window.location.origin + '/gamepad.html', // from [Twitch OAuth Redirect URL](https://glass.twitch.tv/console/apps)
+    redirect: window.location.origin, // from [Twitch OAuth Redirect URL](https://glass.twitch.tv/console/apps)
 };
